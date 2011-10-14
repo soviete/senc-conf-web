ecd78fb2f42d21fb27f696a2529db9986203abf2
kadomu@gmail.com <kadomu@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-14 09:34:38 +0000

    [excelGeneratedOk.php] page shown when list is created added to repo

---
+++ trunk/excelGeneratedOk.php
@@ -0,0 +1,42 @@
+<?php session_start(); ?>
+
+<?php 
+    $sessionName=$_SESSION["sessionName"];
+    $fileName=$_SESSION["nameFile"];
+    //echo "+++++$sessionName\n";
+    echo "======$fileName";
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Generació de llistats</title>
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
+    </head>
+
+    <body>
+        <div  id="wrapper">
+            <?php include 'include/header.php'; ?>
+            <div id="page">
+                <div id="content">
+                    <div id="welcome">
+                        <?php 
+                            print "<h1>Llistat de persones registrades a $sessionName</h1>";
+                            print "<h3>L'arxiu s'ha generat correctament</h3>";
+                            $link="http://localhost/SENCCONFsvn/file/".$fileName;
+                            //echo $link;echo "===================";//del
+                            print "<a href=$link target='_blank'>Baixar l'arxiu</a>";
+                            print "<br><br><p>Per copiar l'arxiu prem el botó dret del teu ratolí, guardar l'arxiu com.</p>";
+                            //echo "===$fileName";//del
+                            //print "<form><input type='button' value='Download Now' onClick='$fileName></form>";//del
+                        ?>
+                         
+                    </div>
+                </div>
+                <div style=" clear: both; height: 1px"></div>
+            </div>
+            <?php include 'include/footer.php'; ?>
+        </div>
+    </body>
+</html>
