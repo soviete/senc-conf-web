5cf62f3ff2dbf5558a2bfd58a357c498598626dc
ramonguixxa@gmail.com <ramonguixxa@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-15 11:29:04 +0000

    Añado DAY & MONTH

--- include/lang.es.php
+++ include/lang.es.php
@@ -28,5 +28,28 @@
     $langVoc['remindChoose'] ='2 - Elige al menos una conferencia esta vez!!!';
     $langVoc['regDetails'] = "DETALLES DE INSCRIPCIÓN";
     $langVoc['noConfSelect'] = 'No has seleccionado ninguna conferencia';
+
+    //DAY & MONTH
+
+$day[0] = "Lunes";
+$day[1] = "Martes";
+$day[2] = "Miércoles";
+$day[3] = "Jueves";
+$day[4] = "Viernes";
+$day[5] = "Sábado";
+$day[6] = "Domingo";
+
+$month[0] = "Enero";
+$month[1] = "Febrero";
+$month[2] = "Marzo";
+$month[3] = "Abril";
+$month[4] = "Mayo";
+$month[5] = "Junio";
+$month[6] = "Julio";
+$month[7] = "Agosto";
+$month[8] = "Septiembre";
+$month[9] = "Octubre";
+$month[10] = "Noviembre";
+$month[11] = "Diciembre";
    
 ?>
