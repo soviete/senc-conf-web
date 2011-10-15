75a1b5e8d168a63a5cfeca3c61521a484549da68
ramonguixxa@gmail.com <ramonguixxa@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-15 11:29:04 +0000

    Añado DAY & MONTH

--- trunk/include/lang.ca.php
+++ trunk/include/lang.ca.php
@@ -28,4 +28,27 @@
     $langVoc['remindChoose'] ='2 - Recorda, tria almenys una conferència aquesta vegada!!!';
     $langVoc['regDetails'] = "DETALLS D'INSCRIPCIÓ";
     $langVoc['noConfSelect'] = 'No has seleccionat cap conferència';
+
+    //DAY & MONTH
+
+$day[0] = "Dilluns";
+$day[1] = "Dimarts";
+$day[2] = "Dimecres";
+$day[3] = "Dijous";
+$day[4] = "Divendres";
+$day[5] = "Dissabte";
+$day[6] = "Diumenge";
+
+$month[0] = "Gener";
+$month[1] = "Febrer";
+$month[2] = "Març";
+$month[3] = "Abril";
+$month[4] = "Maig";
+$month[5] = "Juny";
+$month[6] = "Juliol";
+$month[7] = "Agost";
+$month[8] = "Setembre";
+$month[9] = "Octubre";
+$month[10] = "Novembre";
+$month[11] = "Desembre";
 ?>
--- trunk/include/lang.en.php
+++ trunk/include/lang.en.php
@@ -32,10 +32,6 @@
     $langVoc['noConfSelect'] = 'You have not choose any conference';
     
     
-    
-    
-    
-    
     // Menu
  
     $langVoc['MENU_HOME'] = 'Home';
@@ -44,6 +40,29 @@
     $langVoc['MENU_CONTACT_US'] = 'Contact Us';
     $langVoc['MENU_ADVERTISE'] = 'Advertise';
     $langVoc['MENU_SITE_MAP'] = 'Site Map';
+
+    //DAY & MONTH
+
+$day[0] = "Monday";
+$day[1] = "Tuesday";
+$day[2] = "Wednesday";
+$day[3] = "Thursday";
+$day[4] = "Friday";
+$day[5] = "Sunday";
+$day[6] = "Saturday";
+
+$month[0] = "January";
+$month[1] = "February";
+$month[2] = "March";
+$month[3] = "April";
+$month[4] = "May";
+$month[5] = "June";
+$month[6] = "July";
+$month[7] = "August";
+$month[8] = "September";
+$month[9] = "October";
+$month[10] = "November";
+$month[11] = "December";
 ?>
 
 
