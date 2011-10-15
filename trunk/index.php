cf6877e4b440ba6f1b1e4d8d06db566726139002
kadomu@gmail.com <kadomu@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-15 11:21:15 +0000

    [index.php] header para escoger idioma

--- trunk/index.php
+++ trunk/index.php
@@ -14,33 +14,42 @@ include 'include/common.php';
         <title>Formulari d'inscripció a conferències</title>
         <link rel="stylesheet" type="text/css" href="estilo.css" />
     </head>
+    
+    <div id="lang">
+    <ul>
+        <li><a href="index.php?lang=ca">cat</a></li>
+        <li><a href="index.php?lang=es">esp</a></li>
+        <li><a href="index.php?lang=en"">eng</a></li>
+    </ul>
+    </div>
+    
     <body>
         <div  id="wrapper">
             <?php include 'include/header.php'; ?>
             <div id="page">
                 <div id="content">
                     <div id="welcome">
-                        <h1><?php echo $lang['formTitle']; ?></h1><br>
-                        <h3><?php echo $lang['formSubTitle']; ?></h3>
+                        <h1><?php echo $langVoc['formTitle']; ?></h1><br>
+                        <h3><?php echo $langVoc['formSubTitle']; ?></h3>
                         <form action="ChooseConf.php" method="post">
-                            <div id="UILabel"><?php echo $lang['formName']; ?>*</div>
+                            <div id="UILabel"><?php echo $langVoc['formName']; ?>*</div>
                             <input class="form_tfield" type="text" name="name" value="" /><br><br>
-                            <div id="UILabel"><?php echo $lang['formSurname']; ?>*</div>
+                            <div id="UILabel"><?php echo $langVoc['formSurname']; ?>*</div>
                             <input class="form_tfield" type="text" name="surname" value="" /><br><br>
-                                <div id="UILabel"><?php echo $lang['formId']; ?>*</div>
+                                <div id="UILabel"><?php echo $langVoc['formId']; ?>*</div>
                             <input class="form_tfield" type="text" name="dni" value="" /><br><br>
-                            <div id="UILabel"><?php echo $lang['formEmail']; ?>*</div>
+                            <div id="UILabel"><?php echo $langVoc['formEmail']; ?>*</div>
                             <input class="form_tfield" type="text" name="email" value="" /><br><br>
-                            <div id="UILabel"><?php echo $lang['formRegOption']; ?>*</div>
+                            <div id="UILabel"><?php echo $langVoc['formRegOption']; ?>*</div>
                             <input class="form_tfield" type="text" name="type" value="" /><br><br><br>
                            <div align="right">
-                                <input class="form_submitb" type="submit" name="submit" value=<?php echo $lang['formNextButton']; ?> />
+                                <input class="form_submitb" type="submit" name="submit" value=<?php echo $langVoc['formNextButton']; ?> />
                                 <input type="hidden" name="submitted" value="TRUE" />
                             </div>
                             <br>
                             <div id="note">
                                 <small>
-                                    *<?php echo $lang['mandatoryField']; ?>
+                                    *<?php echo $langVoc['mandatoryField']; ?>
                                 </small>
                             </div>
                         </form>
