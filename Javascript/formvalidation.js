af56c108183311270ddf1bacc4b3890da3842add
ramonguixxa@gmail.com <ramonguixxa@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-15 23:12:54 +0000

    función de validación del primer formulario con Javascript terminada (he tenido que repetir la función que chequea si el campo está vacío o no para todos los campos. No conseguí hacerlo de otra forma.

--- Javascript/formvalidation.js
+++ Javascript/formvalidation.js
@@ -1,8 +1,54 @@
-var x=document.forms["indexform"]["name"].value;
 function validateForm() {
+    //REGEXs
+    cha=/^[a-zA-Z ]+$/ //REGEX for just text characters and spaces
+    anum=/^[0-9a-zA-Z ]+$/ //REGEX to accept alphanumeric and spaces
+    num=/^[0-9]+$/ //REGEX for only numbers
+    //Field Name
+    x=document.forms["indexform"]["name"].value
     if (x==null || x=="") {
-        alert("First name must be filled out");
-        return false;
+        alert("First name must be filled out")
+        return false
+    }
+    if (x.search(cha)==-1) {
+        alert("Please enter only characters in field Name")
+        return false
+    }
+    //Field Surname
+    x=document.forms["indexform"]["surname"].value
+    if (x==null || x=="") {
+        alert("Surname must be filled out")
+        return false
+    }
+    if (x.search(cha)==-1) {
+        alert("Please enter only characters in field Surname")
+        return false
+    }
+    //Field DNI/Passport
+    x=document.forms["indexform"]["dni"].value
+    if (x==null || x=="") {
+        alert("DNI/Passport must be filled out")
+        return false
+    }
+    if (x.search(num)==-1) {
+        alert("DNI/Passport must contain a numeric value")
+        return false
+    }
+    //Field email
+    x=document.forms["indexform"]["email"].value
+    if (x==null || x=="") {
+        alert("email must be filled out")
+        return false
+    }
+    atpos=x.indexOf("@");
+    dotpos=x.lastIndexOf(".")
+    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address")
+        return false
+    }
+    //Field Registration option
+    x=document.forms["indexform"]["type"].value
+    if (x==null || x=="") {
+        alert("Registration option must be filled out")
+        return false
     }
 }
-
