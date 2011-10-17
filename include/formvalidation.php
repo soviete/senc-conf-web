c5d0d7cff0fd434d39ab05c90c2419916de6c45d
ramonguixxa@gmail.com <ramonguixxa@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-17 12:06:21 +0000

    Funciones de php para validar el formulario

---
+++ include/formvalidation.php
@@ -0,0 +1,26 @@
+<?php
+function validChar($name) {
+    if (preg_match("/^[a-zA-Z ]+$/", $name)) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+}
+function validNum($dni) {
+    if (preg_match("/^[0-9]+$/", $dni)) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+}
+function validEmail($email) {
+    if (preg_match("/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/", $email)) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+}
+?>
