852054e1514b22e317b18c37156ca0b0ec8f08a6
ramonguixxa@gmail.com <ramonguixxa@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-17 12:05:55 +0000

    Sólo he añadido dos funciones para redireccionar, he tabulado y he añadido un comentario

--- include/common.php
+++ include/common.php
@@ -1,50 +1,62 @@
 <?php 
+/* ASSIGNING LANGUAGE VARIABLES */
 
-    header('Cache-control: private'); // IE 6 FIX
+header('Cache-control: private'); // IE 6 FIX
 
-    if(isSet($_GET['lang']))
-        {
-            $lang = $_GET['lang'];
+if(isSet($_GET['lang'])) {
+    $lang = $_GET['lang'];
 
-            // register the session and set the cookie
-            $_SESSION['lang'] = $lang;
+    // register the session and set the cookie
+    $_SESSION['lang'] = $lang;
 
-            setcookie('lang', $lang, time() + (3600 * 24 * 30));
-        }
-        
-    else if(isSet($_SESSION['lang']))
-        {
-            $lang = $_SESSION['lang'];
-        }
-    
-    else if(isSet($_COOKIE['lang']))
-        {
-            $lang = $_COOKIE['lang'];
-        }
-        
-    else
-        {
-            $lang = 'ca';
-        }
+    setcookie('lang', $lang, time() + (3600 * 24 * 30));
+}
 
-    switch ($lang) 
-        {
-            case 'en':
-            $lang_file = 'lang.en.php';
-            break;
+else if(isSet($_SESSION['lang'])) {
+    $lang = $_SESSION['lang'];
+}
 
-            case 'ca':
-            $lang_file = 'lang.ca.php';
-            break;
+else if(isSet($_COOKIE['lang'])) {
+    $lang = $_COOKIE['lang'];
+}
 
-            case 'es':
-            $lang_file = 'lang.es.php';
-            break;
+else {
+    $lang = 'ca';
+}
 
-            default:
-            $lang_file = 'lang.ca.php';
+switch ($lang) {
+    case 'en':
+        $lang_file = 'lang.en.php';
+        break;
 
-        }
+    case 'ca':
+        $lang_file = 'lang.ca.php';
+        break;
 
-    include_once 'include/'.$lang_file;
+    case 'es':
+        $lang_file = 'lang.es.php';
+        break;
+
+    default:
+        $lang_file = 'lang.ca.php';
+
+}
+
+include_once 'include/'.$lang_file;
+?>
+
+<?php
+/* Function to get the current page name*/
+function currentPage() {
+    return substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+}
+
+/* Function for redirecting to a different page in the current directory */
+function redirect ($page) {
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $extra = $page;
+    header("Location: http://$host$uri/$extra");
+    exit;
+}
 ?>
