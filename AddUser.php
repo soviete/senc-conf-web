843fad1c46a98bc11dca1047c222f1b99e327d73
kadomu@gmail.com <kadomu@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-19 09:05:01 +0000

    [AddUser.php] [errorNumberSession.php] despues de elegir las conferencias gestioOptions.php te envia o bien a adduser o bien o sino hay sitio a decirte que no te puede apuntar que si quieres te reserva las que esten llenas y te registra en las que hay plaza

--- AddUser.php
+++ AddUser.php
@@ -12,7 +12,7 @@ include 'include/common.php'
         <title>Confirmacio de registre</title>
         <link rel="stylesheet" type="text/css" href="estilo.css" />
     </head>
-
+    
     <body>
         <div  id="wrapper">
             <?php include 'include/header.php'; ?>
@@ -27,92 +27,141 @@ include 'include/common.php'
                         $dni = $_SESSION['dni'];
                         $email = $_SESSION['email'];
                         $type = $_SESSION['type'];
-                        if ($_POST['confs']) {
+                        echo "$lang =======\n";
+                        if ($_SESSION['confs']) {
                             $result = mysql_num_rows(mysql_query("SELECT * FROM USERS WHERE dni='$dni'"));
 
                             if($result == 1) {
                                 //echo '<h3>ERROR!</h3>The username you have chosen already exists!<br>
                                 //Please <a href=index.php>Go back</a> and try another Username';
-                                echo '<h3>ERROR!</h3>Aquest usuari ja està registrat!<br>
-                                    Si us plau, torni a intentar-ho.   <a href=index.php>INSCRIPCIÓ</a>';
+                                echo '<h3>ERROR!</h3>';
+                                echo $langVoc['userRegistered'];
+                                echo '<br>';
+                                echo $langVoc['pleaseRetry'];       
+                                echo '<a href=index.php>';
+                                echo $langVoc['registration'];
+                                echo '</a>';
 
                             }
+                            
                             else {
                                 $query=mysql_query("INSERT INTO USERS (userName, surname, dni, email, type)
                                         VALUES ('$name', '$surname', '$dni', '$email', '$type')");
                                 $user=mysql_fetch_array(mysql_query("SELECT idUser FROM USERS WHERE dni='$dni'"));
                                 $conferences="";
-                                foreach ( $_POST['confs'] as $k=> $c) {
-                                    if ($c == 'on') {
-                                        mysql_query("INSERT INTO REGISTERED (regIDUser, idRegSession)
-                                                VALUES ('$user[0]','$k')");
-                                        $query=mysql_fetch_assoc(mysql_query("SELECT sessionName,
-                                                UNIX_TIMESTAMP(sessionDate),room FROM SESSIONS
-                                                WHERE idSESSIONS='$k'"),MYSQL_NUM);
-                                        $weekdaymysql=date("l",$query[1]);
-                                        $monthmysql=date("F",$query[1]);
-                                        $Day=date("d",$query[1]);
-                                        $year=date("Y",$query[1]);
-                                        $time=date("G",$query[1]);
-                                        $W=$day[$weekdaymysql];
-                                        $M=$month[$monthmysql];
-//                                        foreach ($day as $key => $value) {
+                                
+                                //If three of this sessions are full then bad luck my friend!
+                                $countFullSessions;
+                                
+                                for ($i = 2; $i <= 5; $i++) 
+                                    {
+                                        $queryCount = mysql_query("select count(*) from SENCCONF.REGISTERED WHERE SENCCONF.REGISTERED.idRegSession = '$i'");
+                                        $count = mysql_fetch_array($queryCount);
+                                        
+                                        //$query="SELECT dni, userName, surname, email from SENCCONF.USERS WHERE SENCCONF.USERS.idUser IN (SELECT SENCCONF.REGISTERED.regIdUser FROM SENCCONF.REGISTERED WHERE SENCCONF.REGISTERED.idRegSession = '$idConference')";
+                                        echo $i;echo "=========="; echo "$count[0]"; echo "\n";
+                                        
+                                        if ($count[0] >= 10) 
+                                            {
+                                                $countFullSessions++;
+                                            }
+                                    }
+                                    
+                                echo "Number of session full $countFullSessions\n";
+                                
+                                if ($countFullSessions >= 1)
+                                    {
+                                        echo '<h3>ERROR!</h3>';
+                                        echo $langVoc['noSessionAva'];
+                                        echo '<br>';
+                                        echo $langVoc['waitList'];
+                                        echo '<a href=ChooseConf.php>';
+                                        echo $langVoc['Here'];
+                                        echo '</a>';
+                                        echo '<br>';
+                                        echo $langVoc['asap'];
+                                        
+                                        //include 'include/footer.php';
+                                        //break;
+                                    }
+                                
+                                else
+                                    {    
+                                        foreach ( $_SESSION['confs'] as $k=> $c) {
+                                        
+                                            if ($c == 'on') {
+                                            mysql_query("INSERT INTO REGISTERED (regIDUser, idRegSession)
+                                                         VALUES ('$user[0]','$k')");
+                                            $query=mysql_fetch_assoc(mysql_query("SELECT sessionName,
+                                                                                  UNIX_TIMESTAMP(sessionDate),room FROM SESSIONS
+                                                                                  WHERE idSESSIONS='$k'"),MYSQL_NUM);
+                                            $weekdaymysql=date("l",$query[1]);
+                                            $monthmysql=date("F",$query[1]);
+                                            $Day=date("d",$query[1]);
+                                            $year=date("Y",$query[1]);
+                                            $time=date("G",$query[1]);
+                                            $W=$day[$weekdaymysql];
+                                            $M=$month[$monthmysql];
+    //                                        foreach ($day as $key => $value) {
 //                                            if ($value==$weekdaymysql) {
 //                                                $W=$value;
 //                                            }
-//                                        }
-//                                        foreach ($month as $key => $value) {
+//                                            }
+//                                            foreach ($month as $key => $value) {
 //                                            if ($value==$monthmysql) {
 //                                                $M=$value;
 //                                            }
-//                                        }
-                                        $fecha="$W, $Day of $M $year";
-                                        $conferences.="$fecha<br><i>$query[2]</i><br>\"<b>$query[0]</b>\"<br><br>";
+//                                            }
+                                            $fecha="$W, $Day of $M $year";
+                                            $conferences.="$fecha<br><i>$query[2]</i><br>\"<b>$query[0]</b>\"<br><br>";
+                                        }
+                                    }
+                                    
+                                    if (!$query or !$user) {
+                                    trigger_error ('Wrong QUERY: ' . mysql_error() );
+                                    }
+                                    else {
+                                        echo'<p>Enhorabona <b>',$name,'</b>!! T\'has registrat correctament </p>
+                                             <p>En breu rebràs un email amb la confirmació.</p>';
+                                             $subject = 'Inscripció a ciclo de conferències';
+                                             $message ='<b>'.$name.'</b>, t\'has registrat correctament al cicle de
+                                             conferències <i>El cervell envaeix la ciutat</i>.
+                                             <p>Aquestes són les conferències a les quals t\'has inscrit:</p>
+                                             '.$conferences.'
+                                             <p>Si us plau, confirmi la seva assistència almenys 5 dies abans de la data</p>
+                                            '
+                                            ;
+
+                                            // To send HTML mail, the Content-type header must be set
+                                            $headers  = 'MIME-Version: 1.0' . "\r\n";
+                                            $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+
+                                            // Additional headers
+                                            $headers .= 'From: Conferences <DONOTREPLY@elcervell.com>' . "\r\n";
+                                            $headers .= "Reply-To: Conferences <INFO@elcervell.com>\r\n";
+                                            $headers .= 'Return-Path: Conferences <INFO@elcervell.com>' . "\r\n";
+                                            $headers .= "Organization: Sender Organization\r\n";
+                                            $headers .= "X-Priority: 3\r\n";
+                                            $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
+
+                                            // Mail it
+                                            mail($email, $subject, $message, $headers);
+
+                                        }
                                     }
                                 }
-                                if (!$query or !$user) {
-                                    trigger_error ('Wrong QUERY: ' . mysql_error() );
-                                }
-                                else {
-                                    echo'<p>Enhorabona <b>',$name,'</b>!! T\'has registrat correctament </p>
-                                        <p>En breu rebràs un email amb la confirmació.</p>';
-                                    $subject = 'Inscripció a ciclo de conferències';
-                                    $message ='<b>'.$name.'</b>, t\'has registrat correctament al cicle de
-                                    conferències <i>El cervell envaeix la ciutat</i>.
-                                      <p>Aquestes són les conferències a les quals t\'has inscrit:</p>
-                                      '.$conferences.'
-                                      <p>Si us plau, confirmi la seva assistència almenys 5 dies abans de la data</p>
-                                    '
-                                    ;
-
-                                    // To send HTML mail, the Content-type header must be set
-                                    $headers  = 'MIME-Version: 1.0' . "\r\n";
-                                    $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-
-                                    // Additional headers
-                                    $headers .= 'From: Conferences <DONOTREPLY@elcervell.com>' . "\r\n";
-                                    $headers .= "Reply-To: Conferences <INFO@elcervell.com>\r\n";
-                                    $headers .= 'Return-Path: Conferences <INFO@elcervell.com>' . "\r\n";
-                                    $headers .= "Organization: Sender Organization\r\n";
-                                    $headers .= "X-Priority: 3\r\n";
-                                    $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
-
-                                    // Mail it
-                                    mail($email, $subject, $message, $headers);
-
-                                }
                             }
-                        }
-                        else {
-                            //echo '<h3>ERROR!</h3>A field in the form is missing!<br>
-                            //Please <a href=index.php>Go back</a> and try again.';
-                            echo '<h3>ERROR!</h3>';
-                            echo $langVoc['noConfSelect'];
-                            echo '<br> Si us plau, torni a intentar-ho.
-                                <a href=ChooseConf.php>ENRERE</a>';
-                        }
+                            
+                            else {
+                                //echo '<h3>ERROR!</h3>A field in the form is missing!<br>
+                                //Please <a href=index.php>Go back</a> and try again.';
+                                echo '<h3>ERROR!</h3>';
+                                echo $langVoc['noConfSelect'];
+                                echo '<br> Si us plau, torni a intentar-ho.
+                                     <a href=ChooseConf.php>ENRERE</a>';
+                            }
 
-                        ?>
+                            ?>
 
                     </div>
                 </div>
