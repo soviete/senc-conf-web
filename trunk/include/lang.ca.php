570fed295a7bf40a95dba6111e1dd62cc6ee67c0
ramonguixxa@gmail.com <ramonguixxa@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-23 16:59:32 +0000

    [langs.php] abreviatura CAA

--- trunk/include/lang.ca.php
+++ trunk/include/lang.ca.php
@@ -42,17 +42,17 @@ $langVoc['TermsButton'] = 'Accepto';
 
 $langVoc['regtype0'] = 'Modalidades de inscripción y acreditaciones académicas';
 $langVoc['regtype1'] = '<b>Opción 1</b>: para aquellos que SI deseen recibir una Certificación de
-    Aprovechamiento Académico, de cara a convalidar Créditos de Libre Elección.';
+    Aprovechamiento Académico (CAA), de cara a convalidar Créditos de Libre Elección.';
 $langVoc['regtype2'] = 'La Universidad Autònoma de Barcelona y la Universitat Pompeu Fabra ofrecen 2 Créditos
     de Libre Elección por este programa. Para acceder a ellos, será necesario certificar la asistencia a al
     menos 12 de las sesiones y, para los alumnos de la UAB, además elaborar un trabajo (un ensayo de 3 páginas
     sobre una de las sesiones) que será evaluado por expertos de la SENC. El proceso final de obtención de los
     créditos depende de cada universidad, la información puede encontrarse aquí:';
 $langVoc['regtype3'] = '<b>Opción 2</b>: para aquellos que SI deseen recibir una Certificación de
-    Aprovechamiento Académico, a título personal (la certificación de la SENC tiene valor oficial en sí
+    Aprovechamiento Académico (CAA), a título personal (la certificación de la SENC tiene valor oficial en sí
     misma), será necesario certificar la asistencia a 8 de las sesiones.';
 $langVoc['regtype4'] = '<b>Opción 3</b>: para aquellos que NO deseen recibir una Certificación de
-    Aprovechamiento Académico, os podéis inscribir a tantas sesiones como queráis y el control de
+    Aprovechamiento Académico (CAA), os podéis inscribir a tantas sesiones como queráis y el control de
     asistencia sobre estas plazas tendrá como único objetivo monitorizar el acceso a la sala.';
 
 $langVoc['startReg'] = 'Entrar';
