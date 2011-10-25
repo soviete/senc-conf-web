be040df0cbe28fa17d7ddcf26b040e122909b8c9
ramonguixxa@gmail.com <ramonguixxa@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-25 07:30:16 +0000

    [langs.php][footer] Contact info añadida al footer

--- trunk/include/footer.php
+++ trunk/include/footer.php
@@ -1,4 +1,6 @@
 <div id="footer">
     <p id="legal">EL CERVELL ENVAEIX LA CIUTAT 2011-2012.</p>
+    <p id="legal"><?php echo $langVoc['contact'];?>
+        <a href="mailto:cerebro.invade@gmail.com?subject=Feedback" >cerebro.invade@gmail.com</a></p>
 </div>
 
