dd10da786c0a67c490f5f92d4c897596ea926563
ramonguixxa@gmail.com <ramonguixxa@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-10-28 07:55:29 +0000

    [footer.php] cerebro.ciudad corregido

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
     <p id="legal">EL CERVELL ENVAEIX LA CIUTAT 2011-2012.</p>
     <p id="legal"><?php echo $langVoc['contact'];?>
-        <a href="mailto:cerebro.invade@gmail.com?subject=Feedback" >cerebro.invade@gmail.com</a></p>
+        <a href="mailto:cerebro.ciudad@gmail.com?subject=Feedback" >cerebro.ciudad@gmail.com</a></p>
 </div>
 
