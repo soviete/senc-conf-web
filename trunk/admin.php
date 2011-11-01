201509f82299b9578f897b4bd26930bc6c79beef
kadomu@gmail.com <kadomu@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-11-01 14:51:52 +0000

    [admin.php][regList1.php][regListTable1.php] ahora se puede ver también la gente registrada

--- trunk/admin.php
+++ trunk/admin.php
@@ -22,7 +22,7 @@ include 'include/common.php';
                         <h1>Gestió de conferències</h1>
                         <h3>Llistat d'assistència</h3>
 
-                        <p>Si vols veure quines persones hi ha registrades en una conferència entrar aquí.</p>
+                        <p>Si vols veure quines persones han confirmat la seva assistència a una conferència entrar aquí.</p>
 
                         <form name="list" action="regList.php" method="post">
                             <div align="center">
@@ -37,11 +37,19 @@ include 'include/common.php';
                                 <input class="form_submitb" type="submit" name="submit" value="ENTRAR" />
                             </div>
                         </form><br><br>
+                        
+                        <h3>Llistat de persones registrades</h3>
+                        <p>Si vols veure les persones registrades a una conferència entra aquí</p>
+                        <form name="list" action="regList1.php" method="post">
+                            <div align="center">
+                                <input class="form_submitb" type="submit" name="submit" value="Entrar" />
+                            </div>
+                        </form><br><br>
                         <div id='boxleft'>
                             <input class="form_submitb"type="button" onclick='window.location.href="gestio.php"'
                                    value="<?php echo $langVoc['back1']; ?>">
                         </div>
-
+                        
                     </div>
                 </div>
                 <div style=" clear: both; height: 1px"></div>
---
+++ trunk/regList1.php
@@ -0,0 +1,100 @@
+<?php 
+session_start(); 
+//ini_set('display_errors', 'On');
+//error_reporting(-1);
+include 'include/common.php';   
+?>
+
+<?php 
+//$DBpayUpdate="NO";
+//$_SESSION["DBpayUpdate"]=$DBpayUpdate;
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
+    </head>
+    <body>
+        <div  id="wrapper">
+            <?php include 'include/header.php'; ?>
+
+            <div id="page">
+                <div id="content">
+                    <div id="welcome">
+                        <h1>Llistat de persones registrades</h1><br>
+
+                        <?php
+
+                        include 'mysql_connect.php';
+
+                        //Extracting sessions from table session to show them in a select
+                        $query = "SELECT SESSIONS.sessionNameca, idSESSIONS FROM formulario.SESSIONS ORDER BY idSESSIONS ASC";
+                        $result = mysql_query($query);
+                        $num_rows = mysql_num_rows($result);
+
+                        //echo "que es $num_rows\n";//del
+
+                        if ($num_rows==0) {
+                            echo '<h3>Les sessions no es troben a la base de dades si us plau comunica-ho al teu webmaster.</h3>
+                                          <div id="welcome">
+                                          No hi ha cap persona sessió registrada <b>', $num_rows, '</b>.</div>';
+                        }
+
+                        else {
+                            echo '<h3>Tria la conferència de la qual vols veure el llistat de persones registrades.</h3>';
+                            //echo '<input class="form_tfield" type="text" name="first_name" value="" />';
+                            print "<form action='regListTable1.php' method='post' name='sessions'>";
+                            echo '<br><div id="note">';
+                            print "<div align=center><select multiple name='conference'>";
+
+                            $result = mysql_query($query);
+
+                            $i = 0;
+
+                            while($row = mysql_fetch_array($result)) {
+
+                                $sessionName = $row['sessionNameca'];
+                                $idSession = $row['idSESSIONS'];
+
+                                print "<option value='$idSession'>$sessionName</option>";
+
+                                ++$i;
+                            }
+
+                            print "</select></div>";
+
+                            print "<br><br><br>";
+                            print "
+                                        <div id='boxleft'>
+                                                <input class='form_submitb' onclick='window.location.href=\"admin.php\"'type='button'
+                                                       value=".$langVoc['back1']." />
+                                                </div>
+                                                <div id='boxright'>
+                                                <input class='form_submitb' type='submit' name='submit'
+                                                       value='CARREGAR TABLA' />
+                                                <input type='hidden' name='submitted' value='TRUE' />
+                                                </div>";
+
+                            echo '<br>';
+
+                            print "</form>";
+
+                            echo '</div><br>';
+                        }
+
+
+
+
+                        ?>
+
+                    </div>
+                </div>
+
+                <div style=" clear: both; height: 1px"></div>
+            </div>
+<?php include 'include/footer.php'; ?>
+        </div>
+    </body>
+</html>
