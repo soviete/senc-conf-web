9370a03b39b7f46a195f4b72d995be7125ac949a
kadomu@gmail.com <kadomu@gmail.com@2e1fb3e2-3c7c-2302-eca2-9ba2ffdb860e>
2011-11-06 11:01:16 +0000

    [common.php] funcion de encriptado

--- trunk/include/common.php
+++ trunk/include/common.php
@@ -63,4 +63,27 @@ function redirect ($page) {
 if (currentPage() != "index.php" && empty($_POST) && empty($_SESSION))  {
     header("Location: index.php");
 }
+
+function encrypt($string, $key) {
+   $result = '';
+   for($i=0; $i<strlen($string); $i++) {
+      $char = substr($string, $i, 1);
+      $keychar = substr($key, ($i % strlen($key))-1, 1);
+      $char = chr(ord($char)+ord($keychar));
+      $result.=$char;
+   }
+   return base64_encode($result);
+}
+
+function decrypt($string, $key) {
+   $result = '';
+   $string = base64_decode($string);
+   for($i=0; $i<strlen($string); $i++) {
+      $char = substr($string, $i, 1);
+      $keychar = substr($key, ($i % strlen($key))-1, 1);
+      $char = chr(ord($char)-ord($keychar));
+      $result.=$char;
+   }
+   return $result;
+}
 ?>
